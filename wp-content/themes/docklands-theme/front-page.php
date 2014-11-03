2aef93a70aa4f4b491042e3a9cee7b4821ce5dd1
everaldomatias <matiaseveraldo@gmail.com>
2014-11-03 18:27:06 -0200

    Adiciona arquivo front-page.php

---
+++ wp-content/themes/docklands-theme/front-page.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package Odin
+ * @since 2.2.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="<?php echo odin_full_page_classes(); ?>">
+		<div id="content" class="site-content" role="main">
+
+			Front page
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_footer();
