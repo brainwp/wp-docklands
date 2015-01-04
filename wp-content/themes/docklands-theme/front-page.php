315fb520f5844406bf26531c230dc83a35f33bee
everaldomatias <matiaseveraldo@gmail.com>
2015-01-04 13:18:57 -0200

    Segundo sprint na Home

--- wp-content/themes/docklands-theme/front-page.php
+++ wp-content/themes/docklands-theme/front-page.php
@@ -11,100 +11,146 @@
  */
 
 get_header();
-get_sidebar( 'left' );
 ?>
 
-	<div id="primary" class="col-sm-9">
-		<div id="content" class="site-content" role="main">
+	<div id="primary" class="col-sm-12">
+		<div id="content" class="site-content home" role="main">
 
-			<div class="col-sm-12 slider-home">
-			</div><!-- slider-home -->
+			<?php get_sidebar( 'left' ); ?>
 
-			<h3 class="bg-title">New Arrivals</h3>
-			
-			<div class="col-sm-12 arrivals">
-				<div class="each col-sm-4">
+			<div class="col-sm-9 right">
+
+				<div class="slider-home">
+				</div><!-- slider-home -->
+
+				<h3 class="bg-title">New Arrivals</h3>
+				
+				<div class="col-sm-12 arrivals">
+					<div class="each col-sm-4">
+						<div class="content">
+
+							<div class="thumb">
+							</div><!-- thumb -->
+							<span class="desc">
+								Dododo dododd, dododododod dododododo.
+							</span><!-- desc -->
+							
+							<div class="preco">
+								<span class="string-preco">Our Price </span>
+								<span class="moeda-preco"> £ </span>
+								<span class="o-preco">23,99</span>
+								<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
+							</div><!-- preco -->
+
+							<div class="bottom">
+								<a href="" class="btn cart">
+									Add to cart
+								</a>
+								<a href="" class="btn details pull-right">
+									Details
+								</a>
+							</div><!-- bottom -->
+							
+						</div><!-- content -->
+					</div><!-- each -->
+					<div class="each col-sm-4">
+						<div class="content">
+
+							<div class="thumb">
+							</div><!-- thumb -->
+							<span class="desc">
+								Dododo dododd, dododododod dododododo.
+							</span><!-- desc -->
+							
+							<div class="preco">
+								<span class="string-preco">Our Price </span>
+								<span class="moeda-preco"> £ </span>
+								<span class="o-preco">23,99</span>
+								<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
+							</div><!-- preco -->
+
+							<div class="bottom">
+								<a href="" class="btn cart">
+									Add to cart
+								</a>
+								<a href="" class="btn details pull-right">
+									Details
+								</a>
+							</div><!-- bottom -->
+							
+						</div><!-- content -->
+					</div><!-- each -->
+					<div class="each col-sm-4">
+						<div class="content">
+
+							<div class="thumb">
+							</div><!-- thumb -->
+							<span class="desc">
+								Dododo dododd, dododododod dododododo.
+							</span><!-- desc -->
+
+							<div class="preco">
+								<span class="string-preco">Our Price </span>
+								<span class="moeda-preco"> £ </span>
+								<span class="o-preco">23,99</span>
+								<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
+							</div><!-- preco -->
+
+							<div class="bottom">
+								<a href="" class="btn cart">
+									Add to cart
+								</a>
+								<a href="" class="btn details pull-right">
+									Details
+								</a>
+							</div><!-- bottom -->
+							
+						</div><!-- content -->
+					</div><!-- each -->
+				</div><!-- arrivals -->
+
+			</div><!-- right -->
+
+			<div class="col-sm-12 full">
+
+				<div class="col-sm-6 banner">
 					<div class="content">
-
-						<div class="thumb">
-						</div><!-- thumb -->
-						<span class="desc">
-							Dododo dododd, dododododod dododododo.
-						</span><!-- desc -->
-						
-						<div class="preco">
-							<span class="string-preco">Our Price </span>
-							<span class="moeda-preco"> £ </span>
-							<span class="o-preco">23,99</span>
-							<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
-						</div><!-- preco -->
-
-						<div class="bottom">
-							<a href="" class="btn cart">
-								Add to cart
-							</a>
-							<a href="" class="btn details pull-right">
-								Details
-							</a>
-						</div><!-- bottom -->
-						
+						BANNER
 					</div><!-- content -->
-				</div><!-- each -->
-				<div class="each col-sm-4">
+				</div><!-- banner -->
+
+				<div class="col-sm-6 video">
 					<div class="content">
-
-						<div class="thumb">
-						</div><!-- thumb -->
-						<span class="desc">
-							Dododo dododd, dododododod dododododo.
-						</span><!-- desc -->
-						
-						<div class="preco">
-							<span class="string-preco">Our Price </span>
-							<span class="moeda-preco"> £ </span>
-							<span class="o-preco">23,99</span>
-							<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
-						</div><!-- preco -->
-
-						<div class="bottom">
-							<a href="" class="btn cart">
-								Add to cart
-							</a>
-							<a href="" class="btn details pull-right">
-								Details
-							</a>
-						</div><!-- bottom -->
-						
+						VÍDEO
 					</div><!-- content -->
-				</div><!-- each -->
-				<div class="each col-sm-4">
-					<div class="content">
+				</div><!-- video -->
 
-						<div class="thumb">
-						</div><!-- thumb -->
-						<span class="desc">
-							Dododo dododd, dododododod dododododo.
-						</span><!-- desc -->
+				<?php if ( $differential = get_field( 'home_differential', 'options' ) ) : ?>
+					
+					<div class="differential col-xs-12">
+						<h4>Why Docklands are unique:</h4>
+						<span><?php echo $differential; ?></span>
+					</div><!-- differential -->
 
-						<div class="preco">
-							<span class="string-preco">Our Price </span>
-							<span class="moeda-preco"> £ </span>
-							<span class="o-preco">23,99</span>
-							<span class="imposto-preco">(Inc. Vat £ 28,78)</span>
-						</div><!-- preco -->
+				<?php endif ?>
 
-						<div class="bottom">
-							<a href="" class="btn cart">
-								Add to cart
-							</a>
-							<a href="" class="btn details pull-right">
-								Details
-							</a>
-						</div><!-- bottom -->
-						
-					</div><!-- content -->
-				</div><!-- each -->
-			</div><!-- arrivals -->
+				<?php if ( $banner_2 = get_field( 'home_banner_2', 'options' ) ) : ?>
+					
+					<div class="banner col-sm-6">
+						<img src="<?php echo $banner_2; ?>" alt="">
+					</div><!-- banner -->
+
+				<?php endif ?>
+
+				<?php if ( $banner_3 = get_field( 'home_banner_3', 'options' ) ) : ?>
+					
+					<div class="banner col-sm-6">
+						<img src="<?php echo $banner_3; ?>" alt="">
+					</div><!-- banner -->
+
+				<?php endif ?>
+
+			</div><!-- full -->
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
