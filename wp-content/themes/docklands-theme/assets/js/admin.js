2af227a5ff4a59769921063296e1448abf387dad
matheus <contato@matheusgimenez.com.br>
2015-01-13 19:44:15 -0200

    adicionando imagens das cores no admin

--- wp-content/themes/docklands-theme/assets/js/admin.js
+++ wp-content/themes/docklands-theme/assets/js/admin.js
@@ -1,3 +1,10 @@
 jQuery(document).ready(function($) {
 	$('#taxonomy-color li label:contains("color1")').addClass('color1');
+	$('#taxonomy-color li label:contains("color2")').addClass('color2');
+	$('#taxonomy-color li label:contains("color3")').addClass('color3');
+	$('#taxonomy-color li label:contains("color4")').addClass('color4');
+	$('#taxonomy-color li label:contains("color5")').addClass('color5');
+	$('#taxonomy-color li label:contains("color6")').addClass('color6');
+	$('#taxonomy-color li label:contains("color7")').addClass('color7');
+	$('#taxonomy-color li label:contains("color8")').addClass('color8');
 });
