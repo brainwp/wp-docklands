347c744ddae2f770d8427fff8ceacf3cdf2e6aff
everaldomatias <matiaseveraldo@gmail.com>
2015-02-04 20:07:55 -0200

    Voltando area de comentario na pagina de servicos

--- wp-content/themes/docklands-theme/single-services.php
+++ wp-content/themes/docklands-theme/single-services.php
@@ -27,6 +27,10 @@ get_header(); ?>
 					 */
 					get_template_part( 'content', get_post_format() );
 
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 				endwhile;
 			?>
 		</div><!-- #content -->
