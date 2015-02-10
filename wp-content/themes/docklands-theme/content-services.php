0054390393bbe6a4d3a76f2054c1a9cf189af48c
everaldomatias <everaldo@brasa.art.br>
2015-02-10 19:59:28 -0200

    Ajustes em Services

--- wp-content/themes/docklands-theme/content-services.php
+++ wp-content/themes/docklands-theme/content-services.php
@@ -1,11 +1,11 @@
 <?php
-//content services
+// Content Services
 ?>
 <a class="col-md-4 services each" href="<?php the_permalink(); ?>">
 	<?php the_post_thumbnail('medium'); ?>
 	<h3><?php the_title(); ?></h3>
 	<div class="product-info">
-		<?php _e('View Products'); ?>
+		<?php _e('View Service'); ?>
 		<span class="orange">&gt;</span>
 	</div><!-- .product-info -->
 </a><!-- .col-md-4 services -->
