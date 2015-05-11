d1efd7541a1ebce4a4df166c9d81d21e039d7464
everaldomatias <matiaseveraldo@gmail.com>
2015-05-11 20:41:37 -0300

    Remove pagina com modelo sidebar e sem. Deixando apenas padrao com sidebar

--- wp-content/themes/docklands-theme/page.php
+++ wp-content/themes/docklands-theme/page.php
@@ -1,28 +1,37 @@
 <?php
 /**
- * The template for displaying all pages.
- *
- * This is the template that displays all pages by default.
- * Please note that this is the WordPress construct of pages and that other
- * 'pages' on your WordPress site will use a different template.
+ * The template for displaying pages with sidebar.
  *
  * @package Odin
  * @since 2.2.0
  */
-get_header(); ?>
 
-	<div id="primary" class="<?php echo odin_full_page_classes(); ?>">
+get_header();
+?>
+
+	<div id="primary" class="">
 		<div id="content" class="site-content" role="main">
+
+			<?php get_sidebar( 'left' ); ?>
+
+			<div class="col-sm-9 right">
 
 			<?php
 				// Start the Loop.
 				while ( have_posts() ) : the_post();
-				     the_content();
-				?>
-				<?php
+
+					// Include the page content template.
+					get_template_part( 'content', 'page' );
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) :
+						comments_template();
+					endif;
 				endwhile;
 			?>
 
+			</div><!-- right -->
+			
 		</div><!-- #content -->
 	</div><!-- #primary -->
 
