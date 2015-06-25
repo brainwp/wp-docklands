5b609aec811255cc481093c73dfeae41a31bde91
everaldomatias <matiaseveraldo@gmail.com>
2015-06-25 06:59:14 -0300

    Ajustes nos thumbs de Servicos

--- wp-content/themes/docklands-theme/content-services.php
+++ wp-content/themes/docklands-theme/content-services.php
@@ -2,10 +2,19 @@
 // Content Services
 ?>
 <a class="col-md-4 services each" href="<?php the_permalink(); ?>">
-	<?php the_post_thumbnail('medium'); ?>
-	<h3><?php the_title(); ?></h3>
-	<div class="product-info">
-		<?php _e('View Service'); ?>
-		<span class="orange">&gt;</span>
-	</div><!-- .product-info -->
+	
+	<div class="thumb">
+		<?php the_post_thumbnail('square-thumb'); ?>
+	</div><!-- .thumb -->
+
+	<div class="desc">
+
+		<h3><?php the_title(); ?></h3>
+		<div class="product-info">
+			<?php _e('View Service'); ?>
+			<span class="orange">&gt;</span>
+		</div><!-- .product-info -->
+
+	</div><!-- .desc -->
+
 </a><!-- .col-md-4 services -->
