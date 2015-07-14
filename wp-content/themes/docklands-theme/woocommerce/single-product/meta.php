9a84b552d8a79f8353d0f095a119f4b875199af3
matheus <contato@matheusgimenez.com.br>
2015-07-14 14:16:39 -0300

    issue #53

--- wp-content/themes/docklands-theme/woocommerce/single-product/meta.php
+++ wp-content/themes/docklands-theme/woocommerce/single-product/meta.php
@@ -27,20 +27,66 @@ $tag_count = sizeof( get_the_terms( $post->ID, 'product_tag' ) );
 
 	<?php echo $product->get_tags( ', ', '<span class="tagged_as">' . _n( 'Tag:', 'Tags:', $tag_count, 'woocommerce' ) . ' ', '</span>' ); ?>
 
-	<?php $terms = get_the_terms( $post->ID, 'color' ); ?>
-	<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
-	    <div class="col-md-12 clear"></div><!-- .col-md-12 clear -->
-		<span class="tagged_as">
-			<?php _e('Colors:&nbsp;', 'odin');?>
-			<?php foreach ( $terms as $term ) : ?>
-				<?php if($color_img = get_field('color_img', $term)):?>
-					<a href="<?php echo get_term_link( $term->term_id, 'color' );?>">
-						<img class="color-img" src="<?php echo $color_img['sizes']['thumbnail'];?>">
+	<div class="col-md-12">
+		<?php $terms = get_the_terms( $post->ID, 'color' ); ?>
+		<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
+			<span class="tagged_as">
+				<?php _e('Colors:&nbsp;', 'odin');?>
+				<?php foreach ( $terms as $term ) : ?>
+					<?php if($color_img = get_field('color_img', $term)):?>
+						<a href="<?php echo get_term_link( $term->term_id, 'color' );?>">
+							<img class="color-img" src="<?php echo $color_img['sizes']['thumbnail'];?>">
+						</a>
+			    	<?php endif;?>
+				<?php endforeach; ?>
+			</span>
+		<?php endif;?>
+		<?php $terms = get_the_terms( $post->ID, 'tax_product_type' ); ?>
+		<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
+	    	<span class="tagged_as">
+	    		<?php _e('Product Type:&nbsp;', 'odin');?>
+	    		<?php foreach ( $terms as $term ) : ?>
+	    			<a href="<?php echo get_term_link( $term->term_id, 'tax_product_type' );?>">
+						<?php echo apply_filters( 'the_title', $term->name );?>
 					</a>
-			    <?php endif;?>
-			<?php endforeach; ?>
-		</span>
-	<?php endif;?>
+				<?php endforeach; ?>
+			</span>
+		<?php endif;?>
+	</div><!-- .col-md-12 -->
+	<div class="col-md-12">
+		<?php $terms = get_the_terms( $post->ID, 'material' ); ?>
+		<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
+	    	<span class="tagged_as">
+	    		<?php _e('Material:&nbsp;', 'odin');?>
+	    		<?php foreach ( $terms as $term ) : ?>
+	    			<a href="<?php echo get_term_link( $term->term_id, 'material' );?>">
+						<?php echo apply_filters( 'the_title', $term->name );?>
+					</a>
+				<?php endforeach; ?>
+			</span>
+		<?php endif;?>
+		<?php $terms = get_the_terms( $post->ID, 'brands' ); ?>
+		<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
+	    	<span class="tagged_as">
+	    		<?php _e('Brands:&nbsp;', 'odin');?>
+	    		<?php foreach ( $terms as $term ) : ?>
+	    			<a href="<?php echo get_term_link( $term->term_id, 'brands' );?>">
+						<?php echo apply_filters( 'the_title', $term->name );?>
+					</a>
+				<?php endforeach; ?>
+			</span>
+		<?php endif;?>
+		<?php $terms = get_the_terms( $post->ID, 'product_condition' ); ?>
+		<?php if ( $terms && ! is_wp_error( $terms ) ) : ?>
+	    	<span class="tagged_as">
+	    		<?php _e('Product Condition:&nbsp;', 'odin');?>
+	    		<?php foreach ( $terms as $term ) : ?>
+	    			<a href="<?php echo get_term_link( $term->term_id, 'product_condition' );?>">
+						<?php echo apply_filters( 'the_title', $term->name );?>
+					</a>
+				<?php endforeach; ?>
+			</span>
+		<?php endif;?>
+	</div><!-- .col-md-12 -->
 	<?php do_action( 'woocommerce_product_meta_end' ); ?>
-
 </div>
