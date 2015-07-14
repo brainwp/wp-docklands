e5e5195b6e261e65c79c5d33c33244c4746af5d3
matheus <contato@matheusgimenez.com.br>
2015-07-14 14:27:37 -0300

    issue #54

--- wp-content/themes/docklands-theme/assets/js/admin.js
+++ wp-content/themes/docklands-theme/assets/js/admin.js
@@ -1,11 +1,4 @@
 jQuery(document).ready(function($) {
-	$('#taxonomy-color li label:contains("Color1")').addClass('color1');
-	$('#taxonomy-color li label:contains("Color2")').addClass('color2');
-	$('#taxonomy-color li label:contains("Color3")').addClass('color3');
-	$('#taxonomy-color li label:contains("Color4")').addClass('color4');
-	$('#taxonomy-color li label:contains("Color5")').addClass('color5');
-	$('#taxonomy-color li label:contains("Color6")').addClass('color6');
-	$('#taxonomy-color li label:contains("Color7")').addClass('color7');
-	$('#taxonomy-color li label:contains("Color8")').addClass('color8');
+	$('#product-type option[value=external]').remove();
 });
 
