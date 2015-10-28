4e0f0367fd666f6e2c4c7f705e83fbbe97464d57
matheus <contato@matheusgimenez.com.br>
2015-10-28 11:01:00 -0200

    issue #66

--- wp-content/themes/docklands-theme/inc/custom-taxs.php
+++ wp-content/themes/docklands-theme/inc/custom-taxs.php
@@ -172,6 +172,117 @@ function custom_taxs() {
 	);
 	register_taxonomy( 'product_condition', array( 'product' ), $args );
 
+	$labels = array(
+		'name'                       => _x( 'Door', 'Taxonomy General Name', 'odin' ),
+		'singular_name'              => _x( 'Door', 'Taxonomy Singular Name', 'odin' ),
+		'menu_name'                  => __( 'Door', 'odin' ),
+		'all_items'                  => __( 'All items', 'odin' ),
+		'parent_item'                => __( 'Parent Item', 'odin' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'odin' ),
+		'new_item_name'              => __( 'New Item Name', 'odin' ),
+		'add_new_item'               => __( 'Add New Item', 'odin' ),
+		'edit_item'                  => __( 'Edit Item', 'odin' ),
+		'update_item'                => __( 'Update Item', 'odin' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'odin' ),
+		'search_items'               => __( 'Search Items', 'odin' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'odin' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'odin' ),
+		'not_found'                  => __( 'Not Found', 'odin' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => false,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'door', array( 'product' ), $args );
+
+	$labels = array(
+		'name'                       => _x( 'Drawers/Shelves', 'Taxonomy General Name', 'odin' ),
+		'singular_name'              => _x( 'Drawers/Shelves', 'Taxonomy Singular Name', 'odin' ),
+		'menu_name'                  => __( 'Drawers/Shelves', 'odin' ),
+		'all_items'                  => __( 'All items', 'odin' ),
+		'parent_item'                => __( 'Parent Item', 'odin' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'odin' ),
+		'new_item_name'              => __( 'New Item Name', 'odin' ),
+		'add_new_item'               => __( 'Add New Item', 'odin' ),
+		'edit_item'                  => __( 'Edit Item', 'odin' ),
+		'update_item'                => __( 'Update Item', 'odin' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'odin' ),
+		'search_items'               => __( 'Search Items', 'odin' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'odin' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'odin' ),
+		'not_found'                  => __( 'Not Found', 'odin' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => false,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'drawers_shelves', array( 'product' ), $args );
+
+	$labels = array(
+		'name'                       => _x( 'Format', 'Taxonomy General Name', 'odin' ),
+		'singular_name'              => _x( 'Format', 'Taxonomy Singular Name', 'odin' ),
+		'menu_name'                  => __( 'Format', 'odin' ),
+		'all_items'                  => __( 'All items', 'odin' ),
+		'parent_item'                => __( 'Parent Item', 'odin' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'odin' ),
+		'new_item_name'              => __( 'New Item Name', 'odin' ),
+		'add_new_item'               => __( 'Add New Item', 'odin' ),
+		'edit_item'                  => __( 'Edit Item', 'odin' ),
+		'update_item'                => __( 'Update Item', 'odin' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'odin' ),
+		'search_items'               => __( 'Search Items', 'odin' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'odin' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'odin' ),
+		'not_found'                  => __( 'Not Found', 'odin' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => false,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'format', array( 'product' ), $args );
+
+	$labels = array(
+		'name'                       => _x( 'Adjustaments', 'Taxonomy General Name', 'odin' ),
+		'singular_name'              => _x( 'Adjustaments', 'Taxonomy Singular Name', 'odin' ),
+		'menu_name'                  => __( 'Adjustaments', 'odin' ),
+		'all_items'                  => __( 'All items', 'odin' ),
+		'parent_item'                => __( 'Parent Item', 'odin' ),
+		'parent_item_colon'          => __( 'Parent Item:', 'odin' ),
+		'new_item_name'              => __( 'New Item Name', 'odin' ),
+		'add_new_item'               => __( 'Add New Item', 'odin' ),
+		'edit_item'                  => __( 'Edit Item', 'odin' ),
+		'update_item'                => __( 'Update Item', 'odin' ),
+		'separate_items_with_commas' => __( 'Separate items with commas', 'odin' ),
+		'search_items'               => __( 'Search Items', 'odin' ),
+		'add_or_remove_items'        => __( 'Add or remove items', 'odin' ),
+		'choose_from_most_used'      => __( 'Choose from the most used items', 'odin' ),
+		'not_found'                  => __( 'Not Found', 'odin' ),
+	);
+	$args = array(
+		'labels'                     => $labels,
+		'hierarchical'               => true,
+		'public'                     => true,
+		'show_ui'                    => true,
+		'show_admin_column'          => false,
+		'show_in_nav_menus'          => false,
+		'show_tagcloud'              => true,
+	);
+	register_taxonomy( 'adjustaments', array( 'product' ), $args );
 }
 
 // Hook into the 'init' action
