3dbdf4a6472751b7e32b6963e460294c77f238d2
matheus <contato@matheusgimenez.com.br>
2016-03-22 13:20:08 -0300

    remove odin favicon

--- wp-content/themes/docklands-theme/header.php
+++ wp-content/themes/docklands-theme/header.php
@@ -24,7 +24,6 @@
 	<title><?php wp_title( '|', true, 'right' ); ?></title>
 	<link rel="profile" href="http://gmpg.org/xfn/11" />
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
-	<link href="<?php echo get_template_directory_uri(); ?>/assets/images/favicon.ico" rel="shortcut icon" />
 	<!--[if lt IE 9]>
 	<script src="<?php echo get_template_directory_uri(); ?>/assets/js/html5.js"></script>
 	<![endif]-->
